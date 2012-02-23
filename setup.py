11b870a663dde89838d7194be1219ae60dfa619e
Kyle McCullough <kylemcc@gmail.com>
2012-02-23 16:25:28 -0600

    setting up project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'pylinkedin',
+    version = '0.1.0',
+    author = 'Kyle McCullough',
+    author_email = 'kylemcc@gmail.com',
+    url = 'https://github.com/kylemcc/pylinkedin',
+    description = 'Python library for interacting with the LinkedIn REST API',
+    packages = find_packages(),
+    license = 'MIT License',
+    install_requires = ['httplib2', 'oauth2', 'simplejson']
+)
