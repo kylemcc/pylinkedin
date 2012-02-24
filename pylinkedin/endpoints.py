f83592eadea228bd8c86d912d643993b0cb2713f
Kyle McCullough <kylemcc@gmail.com>
2012-02-24 15:42:36 -0600

    updating endpoints. will make this neat and clean at some point, once I decide how this thing will work

--- pylinkedin/endpoints.py
+++ pylinkedin/endpoints.py
@@ -1,1 +1,16 @@
-GROUP_MEMBERSHIPS = "https://api.linkedin.com/v1/people/~/group-memberships:(group:(id,name),membership-state,show-group-logo-in-profile,allow-messages-from-members,email-digest-frequency,email-announcements-from-managers,email-for-every-new-post)"
+BASE_API = "https://api.linkedin.com/v1/"
+
+"""
+Groups resources
+Documentation: http://developer.linkedin.com/documents/groups
+"""
+
+GROUP_MEMBERSHIPS = BASE_API + "people/~/group-memberships:(group:(id,name,short-description,description,category,website-url,site-group-url,large-logo-url,num-members),membership-state,show-group-logo-in-profile,allow-messages-from-members,email-digest-frequency,email-announcements-from-managers,email-for-every-new-post)"
+
+
+"""
+Social Stream resources
+Documentation: http://developer.linkedin.com/documents/social-stream
+"""
+
+NETWORK_UPDATES = BASE_API + "people/~/network/updates"
