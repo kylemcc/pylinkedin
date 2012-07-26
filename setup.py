d07250c64edc631bbf976336c652d183385d7dbc
Kyle McCullough <kylemcc@gmail.com>
2012-07-26 16:07:45 -0500

    bumping version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'pylinkedin',
-    version = '0.1.0',
+    version = '0.1.1',
     author = 'Kyle McCullough',
     author_email = 'kylemcc@gmail.com',
     url = 'https://github.com/kylemcc/pylinkedin',
